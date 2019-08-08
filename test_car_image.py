ae9d1b0bf4f6561a54efb68cece1047d22123c7e
Gil Dadosh <gildadosh@gmail.com>
2019-08-08 19:42:09 +0300

    threshold test for fast checkpoint

--- test_car_image.py
+++ test_car_image.py
@@ -1,10 +1,11 @@
 import pytest
-import numpy as np
-import time
+import sys
 from detections_framework import DetectionsFramework
 
 #@pytest.mark.incremental
 class TestCarImage:
+	THRESHOLD = 0.00001
+	FILE_WITH_EXPECTED_RESULT = 'expected_result_for_test_car_image.json'
 	# open issues: define init, members and setup (so detect will run once) for now can't define more than 1 test and measure time
 	# def __init__(self, image_path, checkpoint):
 	# 	self.image_path = image_path
@@ -15,8 +16,11 @@ class TestCarImage:
 	# 	detector = DetectionsFramework(self.image_path, self.checkpoint)
 	# 	objects = detector.run()
 
-	def test_detect_multiple_object_with_car_checkpoint(self):
-	 	detector = DetectionsFramework("./images/105118094-GettyImages-926034838.jpg", 'fast')
-	 	objects = detector.run()
-	 	print(objects)
-	 	#assert len(objects1) == 10
+	def test_probs_threshold_with_fast_checkpoint(self):
+		detector = DetectionsFramework("./images/105118094-GettyImages-926034838.jpg", 'fast')
+		unique_name = sys._getframe().f_code.co_name + '_' + detector.get_current_timestamp_as_string()
+		objects = detector.run('105118094-GettyImages-926034838_' + unique_name + '.png')
+		expected = detector.get_expected_from_file(self.FILE_WITH_EXPECTED_RESULT, 'TestCarImage.test_probs_threshold_with_fast_checkpoint')
+		detector.save_test_output(objects, unique_name + ".json")
+		for obj in objects:
+			assert(expected - obj['prob'] <= self.THRESHOLD)
